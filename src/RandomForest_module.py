6a1cc2bdd376ea92db9c875bdca5095f665ff7a7
vozniukivan0501-cmyk <vozniukivan0501@gmail.com>
2026-03-16 22:17:47 +0200

    feat: RandomForest implementation for MNIST

--- src/RandomForest_module.py
+++ src/RandomForest_module.py
@@ -1,15 +1,41 @@
 from MnistClassifierInterface_module import MnistClassifierInterface
+from sklearn.ensemble import RandomForestClassifier
 
 class RandomForest(MnistClassifierInterface):
 
     def __init__(self):
+        """
+        Initialize the random forest classifier.
+        """
 
-        pass
+        self.rf_model = RandomForestClassifier(random_state=42)
+
 
     def train(self, X_train, y_train):
+        """
+        Trains Random Forest model
+        Note: Reshaping 3D input arrays into 2D arrays (n_samples, width * height) to fulfill sklearn input requirements
+            Args:
+                X_train: array-like training data expected shape (n_samples, width, height)
+                y_train: array-like training data expected shape (n_samples,)
+        """
 
-        pass
+        X_train_flat = X_train.reshape(len(X_train), -1)
+        self.rf_model.fit(X_train_flat, y_train)
+
 
     def predict(self, X_test):
+        """
+        Making prediction using trained model
+            Note: Reshaping 3D input arrays into 2D arrays (n_samples, width * height)
+            Args:
+                X_test: array-like test data expected shape (n_samples, width, height)
+            Returns:
+                preds: array-like prediction expected shape (n_samples,)
+        """
 
-        return
+
+        X_test_flat = X_test.reshape(len(X_test), -1)
+        preds = self.rf_model.predict(X_test_flat)
+
+        return preds
