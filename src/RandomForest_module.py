1cc7e2a2a4706fb28535da64734ca2136e7a2122
vozniukivan0501-cmyk <vozniukivan0501@gmail.com>
2026-03-17 00:12:32 +0200

    refactor: RandomForest rf_model renaming to model, minor doc changes

--- src/RandomForest_module.py
+++ src/RandomForest_module.py
@@ -8,7 +8,7 @@ class RandomForest(MnistClassifierInterface):
         Initialize the random forest classifier.
         """
 
-        self.rf_model = RandomForestClassifier(random_state=42)
+        self.model = RandomForestClassifier(random_state=42)
 
 
     def train(self, X_train, y_train):
@@ -17,25 +17,25 @@ class RandomForest(MnistClassifierInterface):
         Note: Reshaping 3D input arrays into 2D arrays (n_samples, width * height) to fulfill sklearn input requirements
             Args:
                 X_train: array-like training data expected shape (n_samples, width, height)
-                y_train: array-like training data expected shape (n_samples,)
+                y_train: array-like training data expected shape (n_samples)
         """
 
         X_train_flat = X_train.reshape(len(X_train), -1)
-        self.rf_model.fit(X_train_flat, y_train)
+        self.model.fit(X_train_flat, y_train)
 
 
     def predict(self, X_test):
         """
-        Making prediction using trained model
+        Classifying using trained model
             Note: Reshaping 3D input arrays into 2D arrays (n_samples, width * height)
             Args:
                 X_test: array-like test data expected shape (n_samples, width, height)
             Returns:
-                preds: array-like prediction expected shape (n_samples,)
+                preds: array-like prediction of integer class values, expected shape (n_samples)
         """
 
 
         X_test_flat = X_test.reshape(len(X_test), -1)
-        preds = self.rf_model.predict(X_test_flat)
+        preds = self.model.predict(X_test_flat)
 
         return preds
